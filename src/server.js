47301d0dc0fd225e229cf76b23d7376d509a516e
YouKnowWhoIs <leokatmax@gmail.com>
2024-06-03 11:32:50 +0300

    up

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ export const setupServer = () => {
   app.get('/contacts/:contactId', async (req, res) => {
     const { contactId } = req.params;
     if (!mongoose.Types.ObjectId.isValid(contactId)) {
-      return res.status(404).json({
+      return res.status(500).json({
         data: 'ID not found',
       });
     }
