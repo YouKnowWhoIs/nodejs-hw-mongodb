7d08580e6522e040cb918b660a4bf42b5aaae574
YouKnowWhoIs <leokatmax@gmail.com>
2024-07-01 20:19:19 +0300

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,93 +1,90 @@
-import mongoose from 'mongoose';
-import createHttpError from 'http-errors';
+import { ONE_DAY } from '../contacts/index.js';
 import {
-  getAllContacts,
-  getOneContacts,
-  createContacts,
-  deleteContacts,
-  updatedContacts,
-} from '../services/contacts.js';
-import { parsePaginationParams } from '../utils/parsePaginationParams.js';
-import { parseSortParams } from '../utils/parseSortParams.js';
-import { parseFilterParams } from '../utils/parseFilterParams.js';
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshUserSession,
+  requestResetToken,
+  resetPassword,
+} from '../services/auth.js';
 
-export const getContctsController = async (req, res) => {
-  const { page, perPage } = parsePaginationParams(req.query);
-  const { sortBy, sortOrder } = parseSortParams(req.query);
-  const filter = parseFilterParams(req.query);
-
-  const contacts = await getAllContacts({
-    page,
-    perPage,
-    sortBy,
-    sortOrder,
-    filter,
-    userId: req.user._id,
+export const setupSessionCookies = (res, session) => {
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
   });
+  res.cookie('sessionToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+};
+
+export const registerUserController = async (req, res) => {
+  const user = await registerUser(req.body);
+
+  res.json({
+    status: 201,
+    message: 'Successfuly registered a user!',
+    data: user,
+  });
+};
+
+export const loginUserController = async (req, res) => {
+  const session = await loginUser(req.body);
+
+  setupSessionCookies(res, session);
 
   res.json({
     status: 200,
-    message: 'Successfully found contacts!',
-    data: contacts,
+    message: 'Successfully logge in an user!',
+    data: {
+      accessToken: session.accessToken,
+    },
   });
 };
 
-export const getContctsByIdController = async (req, res, next) => {
-  const { contactId } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(contactId)) {
-    return res.status(404).json({
-      data: 'ID not found',
-    });
-  }
-
-  const contact = await getOneContacts(contactId);
-  if (!contact) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
-  }
-
-  res.status(200).json({
-    message: `Successfully found contact with id ${contactId}!`,
-    data: contact,
+export const logoutUserController = async (req, res) => {
+  await logoutUser({
+    sessionId: req.cookies.sessionId,
+    sessionToken: req.cookies.sessionToken,
   });
-};
 
-export const createContactController = async (req, res) => {
-  const { body, file } = req;
-
-  const contact = await createContacts({ ...body, photo: file }, req.user._id);
-
-  res.status(201).json({
-    status: 201,
-    message: 'Successfuly create a contact!',
-    data: contact,
-  });
-};
-
-export const updateContactController = async (req, res, next) => {
-  const { contactId } = req.params;
-
-  const result = await updatedContacts(contactId, req.body);
-  if (!result) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
-  }
-
-  res.status(200).json({
-    status: 200,
-    message: 'Successfuly update contact!',
-    data: result.contact,
-  });
-};
-
-export const deleteContactsController = async (req, res, next) => {
-  const { contactId } = req.params;
-
-  const contact = await deleteContacts(contactId);
-  if (!contact) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
-  }
+  res.clearCookie('sessionId');
+  res.clearCookie('sessionToken');
 
   res.status(204).send();
+};
+
+export const refreshUserSessionController = async (req, res) => {
+  const { sessionId, sessionToken } = req.cookies;
+  const session = await refreshUserSession({ sessionId, sessionToken });
+
+  setupSessionCookies(res, session);
+
+  res.json({
+    status: 200,
+    message: 'Successfully refreshe a session!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
+};
+
+export const requestResetEmailControler = async (req, res) => {
+  await requestResetToken(req.body.email);
+
+  res.json({
+    message: 'Reset password email has been successfully sent.',
+    status: 200,
+    data: {},
+  });
+};
+
+export const resetPasswordController = async (req, res) => {
+  await resetPassword(req.boy);
+  res.json({
+    message: 'Password was successfully reset!',
+    status: 200,
+    data: {},
+  });
 };
